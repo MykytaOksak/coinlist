f4fd09ec1a8b7e027f4dafc7247735d17ce87a93
Nikita.Oksak <47701855+MykytaOksak@users.noreply.github.com>
2021-06-30 15:00:14 +0300

    Add files via upload

---
+++ coinlist_qredo.js
@@ -0,0 +1,21 @@
+let run = async () => { 
+    let a = document.body.querySelectorAll(".quiz")[0].querySelectorAll(".c-label--inline"), 
+    b = a => new Promise(b => setTimeout(b, a)), 
+    c = [
+        "It is its own layer 2 blockchain", 
+        "Option 1: 30,000; Option 2: 2,250",
+        "Users in the waiting room will be given a random spot in the queue when the sale starts. Users who arrive after the sale starts will be placed behind those in the waiting room", 
+        "Option 1: Tokens unlock on or around September 8, 2021. Option 2: Tokens release linearly over 12 months starting from January 8, 2022", 
+        "BTC, ETH, USDC, USDT", "BTC, ETH, USDC, USDT", 
+        "Option 1: $0.50 per token, $500 limit. Option 2: $0.225 per token, $1,000 limit", 
+        "The user's purchase may be cancelled and the user may be banned from future CoinList sales", 
+        "CoinList.co", 
+        "coinlistofficialchannel",
+        "The user's account will be terminated and all purchases will be cancelled"
+    ]; 
+    for (let d = 0; d < a.length; d++)-1 !== c.indexOf(a[d].textContent) && 
+    (a[d].querySelectorAll(".c-input")[0].checked = !0), 
+    await b(50 + Math.floor(103 * Math.random()) + 1); 
+    console.log("Done") 
+};    
+run();
